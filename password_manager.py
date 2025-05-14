70d16b2723e422b67e4c2dc3cec04dc389071692
Eswar Gogineni <eswargogineni8126@gmail.com>
2025-05-14 17:06:15 -0500

    Testing

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,58 @@
+import json
+import os
+
+users = []
+DATA_FILE = "/users_data.json"
+
+
+def main():   
+    try:
+        with open(DATA_FILE, "r") as file:
+            users = json.load(file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        users = []  
+    name = input("Enter your name")
+    users.append(name)
+    with open(DATA_FILE, "w") as file:
+        json.dump(users, file, indent=4)
+    # Create an account or have an existing account    
+    #username = input("Enter username")
+    #password = input("Enter Password")
+    
+    # Check if the user exists and if not, force register
+
+    #print("(1) Add new site's information")
+        #print("(1) Ask for site name")
+        #print("(2) Add new username and password")
+
+    #print("(2) Edit a site's information")
+        #print("(1) Change username")
+        #print("(2) Change password")
+
+    #print("(3) View list of saved sites")
+        # print the key value of the dictionary
+        
+    #print("(4) Change account username")
+        # accessing the first value in the array
+
+    #print("(5) Change account password")
+        # accessing the second value in the array
+    
+    #print("(6) Change between accounts")
+        
+    
+    #print("(7) Delete a site's information")
+    
+    #print("(X) Exit")
+    
+    
+ 
+    
+   # print("(4) View list of saved passwords")
+   # print("(5) Delete saved password")
+
+    print(loaded_data)
+    
 
 
 
